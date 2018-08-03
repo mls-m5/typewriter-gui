b8cef73c70faedddbc21a5c4b6bed0628f4f8286
Mattias Lasersköld <mattias.l.sk@gmail.com>
2018-08-03 20:16:57 +0200

    Fixed permissions when mounting

--- usbsave.sh
+++ usbsave.sh
@@ -7,14 +7,14 @@ set -e
 sudo mkdir -p /media/usb-typist-drive
 
 if mount | grep /mnt/sda1 > /dev/null; then
-	sudo mount /dev/sda1 /media/usb-typist-drive/
+	sudo mount /dev/sda1 /media/usb-typist-drive/ ## -o umask=000
 else
     echo "drive already mounted"
 fi
 
 mkdir -p /media/usb-typist-drive/typewriter
 
-cp $1 /media/usb-typist-drive/typewriter
+sudo cp $1 /media/usb-typist-drive/typewriter
 
 sudo umount /media/usb-typist-drive
 
